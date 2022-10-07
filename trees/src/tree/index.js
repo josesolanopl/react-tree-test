c544385945f4a107a39d9cc72655c60eb6f848e9
Jose Solano <jesolano@protonmail.com>
2022-10-07 11:26:28 -0600

    Data modeling completed

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,36 +1,29 @@
 import "./index.css";
+import AnimalTreeData from "./data.json";
 
 export default function AnimalTree() {
+  return <Tree>{buildAnimalNodes(AnimalTreeData, true)}</Tree>;
+}
+
+function buildAnimalNodes({ name, children }, isRoot) {
   return (
-    <Tree>
-      <TreeNode isRoot>
-        <span>mammals</span>
-        <TreeNode>
-          <span>cheetah</span>
-        </TreeNode>
-        <TreeNode>
-          <span>bear</span>
-          <TreeNode>
-            <span>lion</span>
-          </TreeNode>
-          <TreeNode>
-            <span>dog</span>
-            <TreeNode>
-              <span>elephant</span>
-            </TreeNode>
-          </TreeNode>
-        </TreeNode>
-        <TreeNode>
-          <span>ape</span>
-        </TreeNode>
-      </TreeNode>
-    </Tree>
+    <TreeNode isRoot={isRoot}>
+      <span>{name}</span>
+      {children?.length ? children.map(child => buildAnimalNodes(child)) : null}
+    </TreeNode>
   );
 }
 
 const PADDING_VALUE = 2;
 function TreeNode({ isRoot, children }) {
-    return <div class="node" style={{paddingLeft: !isRoot ? `${PADDING_VALUE}rem` : 0}}>{children}</div>
+  return (
+    <div
+      class="node"
+      style={{ paddingLeft: !isRoot ? `${PADDING_VALUE}rem` : 0 }}
+    >
+      {children}
+    </div>
+  );
 }
 
 function Tree({ children }) {
