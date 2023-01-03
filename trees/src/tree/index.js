f9355f6f769c513aa4e1ffc16c061655e2e33a1d
Jose Solano <jesolano@protonmail.com>
2023-01-03 13:25:30 -0700

    On site interview

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,28 +1,45 @@
 import "./index.css";
-import AnimalTreeData from "./data.json";
+import AnimalTreeData2 from "./data.json";
+import AnimalTreeData from "./_data.json";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function AnimalTree() {
-  return <Tree><AnimalNodes animal={AnimalTreeData} isRoot></AnimalNodes></Tree>;
+  console.log()
+  return (
+    <Tree>
+      <AnimalNodes animal={animalDataToAnimal(AnimalTreeData2)}></AnimalNodes>
+    </Tree>
+  );
 }
 
-function AnimalNodes({animal, isRoot}) {
-  const { name } = animal
-  const [children, setChildren] = useState(animal.children)
+function AnimalNodes({ animal, depth = 0, onDelete: propOnDelete }) {
+  const isRoot = depth === 0;
+  const name = `${animal.name[0]}${'.'.repeat(depth)}${animal.name.slice(1)}`
+
+  const [children, setChildren] = useState(animal.children);
 
   const onSubmit = (ev) => {
-    console.log(ev)
-    if (ev.key !== 'Enter') {
-      return
+    console.log(ev);
+    if (ev.key !== "Enter") {
+      return;
     }
 
-    const animalName = ev.target.value
+    const animalName = ev.target.value;
+    setChildren((currState = []) => {
+      const newState = [...currState];
+      newState.push({
+        name: animalName,
+      });
+      return newState;
+    });
+  };
+
+  const onDelete = (animalToDelete) => {
     setChildren((currState = []) => {
       const newState = [...currState]
-      newState.push({
-        name: animalName
-      })
+      const index = currState.findIndex((value) => value === animalToDelete)
+      newState.splice(index, 1)
       return newState
     })
   }
@@ -32,8 +49,18 @@ function AnimalNodes({animal, isRoot}) {
       <div>
         <span>{name}</span>
         <input type="text" onKeyUp={onSubmit} />
+        {!isRoot ? <button onClick={() => propOnDelete?.(animal)}>delete</button> : null}
       </div>
-      {children?.length ? children.map(child => <AnimalNodes animal={child} key={child.name}></AnimalNodes>) : null}
+      {children?.length
+        ? children.map((child) => (
+            <AnimalNodes
+              animal={child}
+              key={child.name}
+              depth={depth + 1}
+              onDelete={onDelete}
+            ></AnimalNodes>
+          ))
+        : null}
     </TreeNode>
   );
 }
@@ -52,4 +79,21 @@ function TreeNode({ isRoot, children }) {
 
 function Tree({ children }) {
   return <div className="tree">{children}</div>;
+}
+
+function animalDataToAnimal(data) {
+  const root = data.find(ad => ad.isRoot)
+  if (!root.children) {
+    return root
+  }
+
+  for (const item of data) {
+    item.children = getInflatedChildren(item, data)
+  }
+
+  return root
+}
+
+function getInflatedChildren(item, dataList) {
+  return item.children?.map(child => dataList.find(dl => dl.id === child)) ?? []
 }
